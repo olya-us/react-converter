f504de651896fd0333f588d3d9d812155877f972
Olya Us <olyaus0809@gmail.com>
2022-11-14 13:30:15 +0300

    Getting a list of exchange rates from a separate API. Convert one currency to another and vice versa (no backend)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+  const rateRef = useRef({});
+
+  useEffect(() => {
+    fetch('http://cdn.cur.su/api/lates.json')
+      .then((res) => res.json)
+      .then((json) => {
+        rateRef = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Не удалось получить информацию');
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rateRef.current[fromCurrency];
+    const result = price * rateRef.current[toCurrency];
+    setToPrice(result.toFixed(3));
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (rateRef.current[fromCurrency] / rateRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block 
+        value={toPrice} 
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
